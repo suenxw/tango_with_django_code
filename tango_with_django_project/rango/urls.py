b026f2ab669c2a52f5f8a4f8290ad0d1342ece09
“Wei <43235151+suenxw@users.noreply.github.com>
2021-07-28 18:10:21 +0100

    chapter 7 solution code(first time)

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -8,8 +8,9 @@ from django.conf.urls import include
 app_name = 'rango'
 
 urlpatterns = [
-
     url(r'^$',views.index,name='index'),
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    url(r'^add_category/$',views.add_category,name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
