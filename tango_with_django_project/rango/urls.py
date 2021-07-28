dca98625a3d94087564d2670e8cd6e6d1272501b
“Wei <43235151+suenxw@users.noreply.github.com>
2021-07-28 16:59:33 +0100

    chapter 6 solution code(first time)

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
 
     url(r'^$',views.index,name='index'),
     path('about/', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category,name='show_category'),
 ]
